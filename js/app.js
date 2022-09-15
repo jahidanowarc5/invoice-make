45136714d55b7a853406305b802202c76d033ee6
user <jahidanowar689@gmail.com>
2022-09-15 10:36:04 +0600

    table-added

--- js/app.js
+++ js/app.js
@@ -8,3 +8,27 @@ document.getElementById('submit-btn').addEventListener('click',function(){
     textInput.value = '';
     
 });
+
+document.getElementById('add-btn').addEventListener('click',function(){
+    const nameInput = document.getElementById('name-input');
+    const priceInput = document.getElementById('price-input');
+    const quantityInput = document.getElementById('quantity-input');
+    const nameInputValue = nameInput.value;
+    const priceInputValue = priceInput.value;
+    const quantityInputValue = quantityInput.value;
+    console.log(nameInputValue,priceInputValue,quantityInputValue)
+    const tableText = document.createElement('tr');
+    tableText.classList.add('item-total')
+     tableText.innerHTML = ` <tr>
+     <th >${nameInputValue}</th>
+     <td>${priceInputValue}</td>
+     <td>${quantityInputValue}</td>
+     <td></td>
+     </tr>` 
+     const tableInfo = document.getElementById('info-table');
+     tableInfo.appendChild(tableText);
+
+
+
+
+})
