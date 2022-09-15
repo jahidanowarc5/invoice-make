97fede268e45d0c819c83be175cff4cda8b6f036
user <jahidanowar689@gmail.com>
2022-09-15 09:20:57 +0600

    buyer-info-done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 
 
+document.getElementById('submit-btn').addEventListener('click',function(){
+    const textInput = document.getElementById('text-field');
+    const textInputValue = textInput.value;
+    const buyerInfo = document.getElementById('buyer-info');
+    buyerInfo.innerText = textInputValue;
+    textInput.value = '';
+    
+});
